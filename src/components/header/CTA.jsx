1429f4afe3fa42ceb07cfda70a8fb38b5bb735d0
Kothuru Abhilash Reddy <kothuruabhilashreddy@gmail.com>
2023-01-14 15:03:51 +0530

    v1.1 includes UI enhancment and data passing.

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import CV from '../../assets/test.pdf'
+import CV from '../../assets/Resume.pdf'
 function CTA() {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn' >Download CV</a>
+        <a href={CV} download className='btn btn-primary' >Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
--- src/components/header/headersocials.jsx
+++ src/components/header/headersocials.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {BsLinkedin} from 'react-icons/bs';
-import {FaGithub} from 'react-icons/fa';
+import {FaGithub, FaInstagram} from 'react-icons/fa';
 
 function headersocials() {
   return (
     <div className='header header__socials'>
-      <a href="https://www.linkedin.com" target="__blank"><BsLinkedin /></a>
-      <a href="https://www.github.com" target="__blank"><FaGithub /></a>
-      <a href="https://www.linkedin.com" target="__blank">##</a>
+      <a href="https://www.linkedin.com/in/kothuru-abhilash-reddy-976649146/" target="__blank" id="social_icon"><BsLinkedin /></a>
+      <a href="https://github.com/kothuruabhilashreddy" target="__blank" id="social_icon"><FaGithub /></a>
+      <a href="https://www.instagram.com/abhiireddy/" target="__blank" id="social_icon"><FaInstagram /></a>
     </div>
   )
 }
 
-export default headersocials
+export default headersocials;
+
